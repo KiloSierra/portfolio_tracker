cc6d439e9e56e24d12964da010fbdb4db28155ed
Alex Bilimoria <acbilimoria@gmail.com>
2015-08-12 22:35:03 -0400

    Generate User model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150811220514) do
+ActiveRecord::Schema.define(version: 20150813023340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,11 @@ ActiveRecord::Schema.define(version: 20150811220514) do
   create_table "stocks", force: :cascade do |t|
     t.string   "name"
     t.string   "ticker"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
