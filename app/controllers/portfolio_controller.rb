c81d20efc546a85f5aa613fdb5908c92e31a65a5
Alex Bilimoria <acbilimoria@gmail.com>
2015-08-12 22:15:11 -0400

    Build add stock function and route to be called via AJAX.

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -2,4 +2,10 @@ class PortfolioController < ApplicationController
   def index
     @stock = Stock.all.order("ASC ticker")
   end
+
+  def add_stock
+    stock = Stock.new
+    stock.ticker = params[:stock_ticker]
+    stock.save
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
  root 'portfolio#index'
+ match 'add_stock', to: 'portfolio#add_stock', via: :post
 end
